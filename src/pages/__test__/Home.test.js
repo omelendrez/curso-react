9fed2e34a001f5fa24c7a0720eccf3f9ff934ece
omelendrez <omar.melendrez@gmail.com>
2021-09-08 17:32:12 -0300

    Implementar unit test

---
+++ src/pages/__test__/Home.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
+import Home from "../Home";
+//import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+describe("Testing Login page", () => {
+  it("It should match snapshot", () => {
+    const component = renderer
+      .create(
+        <MemoryRouter>
+          <Home />
+        </MemoryRouter>
+      )
+      .toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+});
